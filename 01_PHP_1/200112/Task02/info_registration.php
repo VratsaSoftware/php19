bf23baeb374ff50732fde9106cd7cbd22c3a2833
Milena Tomova <kokolina1888@gmail.com>
2020-01-12 15:48:20 +0200

    sinc

--- 01_PHP_1/200112/Task02/info_registration.php
+++ 01_PHP_1/200112/Task02/info_registration.php
@@ -1,30 +1,33 @@
 <?php
-if (!empty($_POST["firstname"]) && !empty($_POST["lastname"]) && !empty($_POST["family"]) && !empty($_POST["ern"]) && !empty($_POST["address"]) && !empty($_POST["education"]) && !empty($_POST["profession"])) {
+var_dump($_POST);
+if (!empty($_POST["firstname"]) && !empty($_POST["Last_name"]) && !empty($_POST["family"]) && !empty($_POST["ern"]) && !empty($_POST["address"]) && !empty($_POST["education"]) && !empty($_POST["profession"])) {
     
     $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
+    $lastname = $_POST["Last_name"];
     $family = $_POST["family"];
     $ern = $_POST["ern"];
     $address = $_POST["address"];
     $education = $_POST["education"];
     $profession = $_POST["profession"];
-}
     echo 
 	"<ul>"
-	. "<li> First name -" . $firstname["firstname"] . "</li>"
-	. "<li> Last name - " . $lastname["lastname"] . "</li>"
-    . "<li> Family - " . $_POST["family"] . "</li>"
-    . "<li> ERN - " . $ern["ern"] . "</li>"
-    . "<li> Address - " . $address["address"] . "</li>"
-    . "<li> Education - " . $education["education"] . "</li>"
-    . "<li> Profession - " . $profession["profession"] . "</li>"
-    . "</ul>"
-
-if (empty($_POST["firstname"]) && empty($_POST["lastname"]) && empty($_POST["family"]) && empty($_POST["ern"]) && empty($_POST["address"]) && empty($_POST["education"]) && empty($_POST["profession"])) {
-
+	. "<li> First name -" . $firstname . "</li>"
+	. "<li> Last name - " . $lastname . "</li>"
+    . "<li> Family - " . $family . "</li>"
+    . "<li> ERN - " . $ern . "</li>"
+    . "<li> Address - " . $address . "</li>"
+    . "<li> Education - " . $education . "</li>"
+    . "<li> Profession - " . $profession . "</li>"
+    . "</ul>";
+} else {
      echo "Fill out the form!";
-}   else {
-    echo "I don`t know what happened!";
 }
+
+// if (empty($_POST["firstname"]) && empty($_POST["lastname"]) && empty($_POST["family"]) && empty($_POST["ern"]) && empty($_POST["address"]) && empty($_POST["education"]) && empty($_POST["profession"])) {
+
+//      echo "Fill out the form!";
+// }   else {
+//     echo "I don`t know what happened!";
+// }
 
 ?>
