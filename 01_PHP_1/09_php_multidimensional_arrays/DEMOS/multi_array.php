399881317ab410adb606a590d6b9cf40550457ea
Milena Tomova <kokolina1888@gmail.com>
2020-01-19 14:27:53 +0200

    sinc

---
+++ 01_PHP_1/09_php_multidimensional_arrays/DEMOS/multi_array.php
@@ -0,0 +1,5 @@
+<?php 
+
+$car1 = ['brand' => 'Volvo', 'model' => 'ws', 'price' => 20000];
+$car2 = ['brand' => 'Fiat', 'model' => 'model1', 'price' => 15000];
+$car = ['brand' => 'BMW', 'model' => 'model2', 'price' => 25000];
