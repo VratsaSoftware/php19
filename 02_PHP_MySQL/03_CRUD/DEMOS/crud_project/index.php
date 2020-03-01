039dd0880ca1895d74916a957f2dbca34e3d8c70
Your Namemiletomova <mile.tomova@gmail.com>
2020-03-01 16:40:09 +0200

    sinc

--- 02_PHP_MySQL/03_CRUD/DEMOS/crud_project/index.php
+++ 02_PHP_MySQL/03_CRUD/DEMOS/crud_project/index.php
@@ -8,10 +8,13 @@ $result = mysqli_query($conn, $read_query);
 
 if( mysqli_num_rows($result) > 0 ){
 ?>
-<table border="1">
+<table style="margin-left: 50px" class="table table-striped">
 	<tr>
 		<td>#</td>
 		<td>unit</td>
+		<td>***</td>
+		<td>soft delete</td>
+		
 	</tr>
 <?php
 $num = 1;
@@ -19,12 +22,16 @@ $num = 1;
 		?>
 		<tr>
 			<td><?= $num ++?></td>
-			<td><?= $row['unit_name'] ?></td>		
+			<td><?= $row['unit_name'] ?></td>	
+			<td><a href="update.php?id=<?= $row['unit_id'] ?>" class="btn btn-warning">UPDATE</a></td>	
+			<td><a href="soft_delete.php?id=<?= $row['unit_id'] ?>">SOFT DELETE</a></td>	
 		</tr>
 		<?php
 	}
 ?>
 </table>
+
+<a href="recycle_bin.php">Recycle Bin</a>
 <?php
 
 } else {
