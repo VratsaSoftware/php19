a2ca03bd9d92298b5091d80bd99af70b052cf323
Your Namemiletomova <mile.tomova@gmail.com>
2020-05-21 08:49:24 +0300

    SINC

---
+++ 03_JavaScript/01_intro/DEMOS/script.js
@@ -0,0 +1,32 @@
+var firstName = 'Milena', 
+	lastName = 'Tomova', numA = '22', 
+	numB = '1',
+	fullName,
+	address, city, streetNum, arr, newArr;
+
+arr = [1, 2, 3];
+streetNum = 10;
+
+// console.log(firstName.length )
+// console.log( streetNum.length )
+// console.log( arr.length )
+
+newArr = lastName.split('');
+// console.log(newArr.join())
+// console.log(newArr);
+
+// console.log(lastName.join())
+
+fullName = firstName + '\'' + lastName;
+
+// console.log(fullName)
+
+console.log( +'1'+(+'1') )
+
+
+console.log(typeof(+'1'))
+// console.log( +numA + (+numB))
+
+
+
+
