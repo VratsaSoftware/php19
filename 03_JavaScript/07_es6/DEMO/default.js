69d6723c8b2d70ce10f9f844d602985e5a1962e8
Your Namemiletomova <mile.tomova@gmail.com>
2020-06-14 14:50:41 +0300

    sinc

---
+++ 03_JavaScript/07_es6/DEMO/default.js
@@ -0,0 +1,6 @@
+
+var x = function( x, y = 10, z = 10 ) {
+   return x * y / z;
+}
+console.log(x(5))//30
+console.log((5, 7))
---
+++ 03_JavaScript/07_es6/DEMO/findIndex.js
@@ -0,0 +1,8 @@
+let numbers = [4, 9, 16, 25, 29];
+let first = numbers.findIndex(myFunction);//3
+
+function myFunction(value, index, array) {
+  return value > 18;
+} 
+
+console.log(first)
