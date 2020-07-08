2fc146112af59848fa00c68a5789d40ec0137364
Your Namemiletomova <mile.tomova@gmail.com>
2020-07-08 18:57:22 +0300

    init

---
+++ 03_JavaScript/14_ajax_lab/DEMO/includes/header.php
@@ -0,0 +1,30 @@
+<?php 
+include 'includes/db_connect.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<style type="text/css">
+		.colored-star{
+			color: #FDCB04;			
+		}
+			
+		span {
+			font-size: 40px
+		}
+		span:hover {
+			cursor: grab
+		}
+	</style>
+</head>
+<body>
+
+<header>
+	<h1 class="text-center">
+		Ajax Demo
+	</h1>
+</header>
