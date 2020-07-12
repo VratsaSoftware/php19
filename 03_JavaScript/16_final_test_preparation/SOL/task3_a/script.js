ea0a236101b354d8d84b0c6771a42fe183aa3582
Your Namemiletomova <mile.tomova@gmail.com>
2020-07-12 15:55:10 +0300

    sinc

--- 03_JavaScript/16_final_test_preparation/SOL/task3_a/script.js
+++ 03_JavaScript/16_final_test_preparation/SOL/task3_a/script.js
@@ -2,17 +2,23 @@ $('#btn').on('click', function(e){
 	e.preventDefault();
 	$('.error').remove();
 	let username = $('#name').val(), 
-		password = $('#pd').val(), flag;
+		password = $('#pd').val(), flag = true;
 
 		//validation
 		if( !username.trim() ){
 			$('#name')
 			.before('<p class="error">Field Required</p>');
+			flag = false;
 		}
 
 		if( password.trim().length < 6 ){
 			$('#pd')
 			.before('<p class="error">Password must be at least 6 characters long!</p>');
+			flag = false;
+		}
+
+		if( flag ){
+			alert('Success!')
 		}
 
 })
