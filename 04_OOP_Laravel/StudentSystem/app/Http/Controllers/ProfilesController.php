0663811be67d0dff376482902e9eca2981fc030b
kokolina1888 <kokolina1888@gmail.com>
2020-11-08 21:49:58 +0200

    Fixed profile image update and display

--- 04_OOP_Laravel/StudentSystem/app/Http/Controllers/ProfilesController.php
+++ 04_OOP_Laravel/StudentSystem/app/Http/Controllers/ProfilesController.php
@@ -37,27 +37,34 @@ class ProfilesController extends Controller
         //dd($request->file('image'));
 
         //get from $_FILES
-        // $file = $request->file('image');      
+        $file = $request->file('image');      
         
         //input type text, name = file
-        $file = $request->file;
+        // $file = $request->file;
 
         // Storage::put('public/file.jpg', $file);
         
         // dd($file);          
         // $file = 'text';
         //creates new file, must have a file content
-        Storage::put('public/file.doc', $file );
+        // Storage::put('public/file.doc', $file );
         
         // $path = $request->file('image')->store('user_images');
 
 
         // // $path = Storage::putFile('user_images', $request->file('image'));
-        // $ext = $request->file('image')->getClientOriginalExtension();
+       
+        $ext = $request->file('image')->getClientOriginalExtension();
 
-        // $path = $request->file('image')
-        //     ->storeAs('user_images', $request->user()->name .'.' . $ext);
-        //     // ->storeAs('user_images', $request->user()->id .'.' . $ext);
+        $path = $request->file('image')
+            ->storeAs('public/user_images', $request->user()->name .'.' . $ext);
+        
+        $profile = Profile::find( $profile )->first();
+
+        $profile->image = 'user_images/' . $request->user()->name .'.' . $ext;
+
+        $profile->save();
+            // ->storeAs('user_images', $request->user()->id .'.' . $ext);
         //     dd($path);
 
        // $name = $request->file('image')->getClientOriginalName();
@@ -69,5 +76,6 @@ class ProfilesController extends Controller
             //other sources and not to be hardcoded here
       
         // Storage::delete('user_images/107.png');
+        return redirect()->back()->with('message', 'Profile updated!');
     }
 }
--- 04_OOP_Laravel/StudentSystem/resources/views/profiles/edit.blade.php
+++ 04_OOP_Laravel/StudentSystem/resources/views/profiles/edit.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
+@if( Session::has('message') )
+<div class="alert alert-success">
+	<p>{{ Session::get('message') }}</p>
+</div>
+@endif
+
 {!! Form::model( $profile,  ['route' => ['profiles.update', $profile->id], 
 							'method' => 'put', 
 							'enctype' => 'multipart/form-data']) !!}
@@ -9,12 +15,15 @@
 	<h1>{{ $user_data->name }}</h1>
 	<p>{{ $profile->bio }}</p>
 	
-	Image here
-	<img src="">
+	
+	@if( !empty( $profile->image ) )
+
+	<img src="{{ asset( 'storage/' . $profile->image ) }}" width="200px">
+
+	@endif
 	<div class="row">
 		Add a Photo Of You: 
 		{!! Form::file('image') !!}	
-		{!! Form::text('file') !!}	
 	</div>
 	<div class="form-group">
 		{!! Form::submit('edit', ['class' => 'btn btn-success']) !!}
